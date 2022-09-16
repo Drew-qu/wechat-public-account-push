6d8ff34eb039b79e188b0cddf56c5b88fc269fea
qudu <qud8277@163.com>
2022-09-17 00:01:01 +0800

    调试完成

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wxa18ca1465ee82af4",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "ca25aed6b77469b3ed3d51ee6eb886c7",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -34,9 +34,9 @@ export const config = {
     // 土味情话(彩虹屁)
     earthyLoveWords: true,
     // 朋友圈文案
-    momentCopyrighting: false,
+    momentCopyrighting: true,
     // 毒鸡汤
-    poisonChickenSoup: false,
+    poisonChickenSoup: true,
     // 古诗古文
     poetry: false,
 
@@ -74,7 +74,7 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://weibo.com",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -94,18 +94,42 @@ export const config = {
         // {"keyword": "ex_day", date: "2022-09-10"},
       ]
     },
-    // {
-    //   name: "傻狗鄢凤琪",
-    //   id: "oI2Ej6WlVC1IswBXtLijFQzbiGPw",
-    //   useTemplateId: "l-kdASWS58zE4oF-PQjUVQFUV9m7tznyS_vmxGy28Yc",
-    //   province: "浙江",
-    //   city: "杭州",
-    //   horoscopeDate: '02-03',
-    //   horoscopeDateType: '本月',
-    //   openUrl: "https://wangxinleo.cn",
-    //   festivals: [],
-    //   customizedDateList: []
-    // },
+    {
+      // 想要发送的人的名字
+      name: "阿度同学",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "oI2Ej6ZBLgBmIq3zw1hF40IAlbDg",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "6a5aCZiPL6aBXCDdiB-lyhfhuxDlAjH0rCMkpg4BH-0",
+      // 所在省份
+      province: "浙江",
+      // 所在城市
+      city: "杭州",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '01-21',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://weibo.com",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "生日", "name": "自己", "year": "1997", "date": "01-21"},
+        // {"type": "节日", "name": "恋爱纪念日", "year": "2021", "date": "02-06"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        // {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+        // {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 认识的日子
+        {"keyword": "know_day", date: "2020-04-28"},
+        // 在一起的日子
+        {"keyword": "love_day", date: "2021-02-06"},
+        // 退伍日
+        // {"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
     // {
     //   name: "老婆2",
     //   id: "",
